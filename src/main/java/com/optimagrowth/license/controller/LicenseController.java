f335ff7cdda6bb41d79cf12420edb20db01db423
chocolithm <kys8336@gmail.com>
2025-02-18 19:21:59 +0900

    spring-hateoas 도입 / 추가로 찾아볼 것

--- src/main/java/com/optimagrowth/license/controller/LicenseController.java
+++ src/main/java/com/optimagrowth/license/controller/LicenseController.java
@@ -1,13 +1,14 @@
 package com.optimagrowth.license.controller;
 
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
 import com.optimagrowth.license.model.License;
 import com.optimagrowth.license.service.LicenseService;
 import java.util.Locale;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.MessageSource;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -28,7 +29,21 @@ public class LicenseController {
   public ResponseEntity<License> getLicense(
       @PathVariable("organizationId") String organizationId,
       @PathVariable("licenseId") String licenseId) {
+
     License license = licenseService.getLicense(licenseId, organizationId);
+    license.add(linkTo(methodOn(LicenseController.class)
+            .getLicense(organizationId, license.getLicenseId()))
+            .withSelfRel(),
+        linkTo(methodOn(LicenseController.class)
+            .createLicense(organizationId, license, null))
+            .withRel("createLicense"),
+        linkTo(methodOn(LicenseController.class)
+            .updateLicense(organizationId, license))
+            .withRel("updateLicense"),
+        linkTo(methodOn(LicenseController.class)
+            .deleteLicense(organizationId, license.getLicenseId()))
+            .withRel("deleteLicense"));
+
     return ResponseEntity.ok(license);
   }
 
@@ -43,7 +58,7 @@ public class LicenseController {
   public ResponseEntity<String> createLicense(
       @PathVariable("organizationId") String organizationId,
       @RequestBody License request,
-      @RequestHeader(value="Accept-Language", required = false) Locale locale) {
+      @RequestHeader(value = "Accept-Language", required = false) Locale locale) {
     return ResponseEntity.ok(licenseService.createLicense(request, organizationId, locale));
   }
 
