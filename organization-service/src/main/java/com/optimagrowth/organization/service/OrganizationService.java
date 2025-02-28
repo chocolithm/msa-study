4adb2462636bc2ac86621adbc521e46da7e472ea
chocolithm <kys8336@gmail.com>
2025-03-01 02:12:20 +0900

    메시징 마무리

--- organization-service/src/main/java/com/optimagrowth/organization/service/OrganizationService.java
+++ organization-service/src/main/java/com/optimagrowth/organization/service/OrganizationService.java
@@ -1,7 +1,5 @@
 package com.optimagrowth.organization.service;
 
-import com.optimagrowth.organization.events.source.SimpleSourceBean;
-import com.optimagrowth.organization.utils.ActionEnum;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -10,49 +8,52 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.optimagrowth.organization.events.source.SimpleSourceBean;
 import com.optimagrowth.organization.model.Organization;
 import com.optimagrowth.organization.repository.OrganizationRepository;
+import com.optimagrowth.organization.utils.ActionEnum;
 
 @Service
 public class OrganizationService {
-	
-	private static final Logger logger = LoggerFactory.getLogger(OrganizationService.class);
-	
-    @Autowired
-    private OrganizationRepository repository;
 
-    @Autowired
-    SimpleSourceBean simpleSourceBean;
+  private static final Logger logger = LoggerFactory.getLogger(OrganizationService.class);
 
-    public Organization create(Organization organization){
-      organization.setId(UUID.randomUUID().toString());
-      organization = repository.save(organization);
-      simpleSourceBean.publishOrganizationChange(
-          ActionEnum.CREATED,
-          organization.getId());
-      return organization;
+  @Autowired
+  private OrganizationRepository repository;
 
+  @Autowired
+  SimpleSourceBean simpleSourceBean;
+
+  public Organization findById(String organizationId) {
+    Optional<Organization> opt = repository.findById(organizationId);
+    simpleSourceBean.publishOrganizationChange(ActionEnum.GET, organizationId);
+    return (opt.isPresent()) ? opt.get() : null;
+  }
+
+  public Organization create(Organization organization){
+    organization.setId( UUID.randomUUID().toString());
+    organization = repository.save(organization);
+    simpleSourceBean.publishOrganizationChange(ActionEnum.CREATED, organization.getId());
+    return organization;
+
+  }
+
+  public void update(Organization organization){
+    repository.save(organization);
+    simpleSourceBean.publishOrganizationChange(ActionEnum.UPDATED, organization.getId());
+  }
+
+  public void delete(String organizationId){
+    repository.deleteById(organizationId);
+    simpleSourceBean.publishOrganizationChange(ActionEnum.DELETED, organizationId);
+  }
+
+  @SuppressWarnings("unused")
+  private void sleep(){
+    try {
+      Thread.sleep(3000);
+    } catch (InterruptedException e) {
+      logger.error(e.getMessage());
     }
-
-    public Organization findById(String organizationId) {
-    	Optional<Organization> opt = repository.findById(organizationId);
-        return (opt.isPresent()) ? opt.get() : null;
-    }
-
-    public void update(Organization organization){
-    	repository.save(organization);
-    }
-
-    public void delete(String organizationId){
-    	repository.deleteById(organizationId);
-    }
-    
-    @SuppressWarnings("unused")
-	private void sleep(){
-		try {
-			Thread.sleep(3000);
-		} catch (InterruptedException e) {
-			logger.error(e.getMessage());
-		}
-	}
+  }
 }
